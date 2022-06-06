3bcd3c66b1bad8a1321dd6428bd6314035824022
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-07 01:21:05 +0600

    :hammer: Remove execute file

--- pipcx/commands/execute.py
+++ pipcx/commands/execute.py
@@ -2,7 +2,7 @@ from pipcx.base import Base
 
 
 class Command(Base):
+    short_description = "Test Case"
 
-    def execute(self):
-
-        print("Hello World")
+    def execute(self, *args, **kwargs):
+        print("Hello Worlds")
