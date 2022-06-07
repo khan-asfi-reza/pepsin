8820330a5207a26129960a0505223cffa0cffbae
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-07 14:51:53 +0600

    :recycle: Code refactor

--- pipcx/commands/init.py
+++ pipcx/commands/init.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 
 from pipcx.base import Base
+from pipcx.error import InvalidCommandError
 from pipcx.utils.venv import install_venv, initialize_venv, activate_venv
 
 
@@ -9,11 +10,39 @@ class Command(Base):
 
     def add_argument(self, parser: ArgumentParser):
         parser.add_argument(
+            "virtualenv",
+            nargs='?',
+            metavar="virtualenv",
+            type=str,
+            help="Name of virtualenv directory",
+            default=''
+
+        )
+
+        parser.add_argument(
             '--venv', help="Virtual Environment directory name"
         )
 
     def execute(self, *args, **kwargs):
-        venv_dir = kwargs.get("venv", "venv")
+        venv_dir = kwargs.get("venv", None)
+        virtual_env = kwargs.get("virtualenv", None)
+
+        # Only one argument is allowed
+        if virtual_env and venv_dir:
+            raise InvalidCommandError("2 Virtualenv directory name provided, "
+                                      "use either pipcx init 'name' or pipcx init --venv='name'")
+
+        elif virtual_env and not venv_dir:
+            name = virtual_env
+
+        elif venv_dir and not virtual_env:
+            name = venv_dir
+
+        else:
+            name = 'venv'
+        # Installs virtualenv library
         install_venv()
-        initialize_venv(venv_dir)
-        activate_venv(venv_dir)
+        # Initializes virtualenv
+        initialize_venv(name)
+        # Activates virtualenv
+        activate_venv(name)
