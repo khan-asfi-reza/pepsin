217b61ed264fa22b3237c1f81a42eb5a2158c5b5
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-10 18:38:55 +0600

    :sparkles: Config input prompt #33

--- pipcx/commands/init.py
+++ pipcx/commands/init.py
@@ -1,8 +1,33 @@
+import os
 from argparse import ArgumentParser
 
 from pipcx.base import Base
-from pipcx.error import InvalidCommandError
+from pipcx.io import PromptHandler, Input
+from pipcx.utils import YamlConfigGenerator
+from pipcx.utils.spinner import Spinner
 from pipcx.utils.venv import install_venv, initialize_venv, activate_venv
+
+main_file = """
+# Generated with pipcx
+
+if __name__ == "__main__":
+   print("Egg and spam")
+"""
+
+
+def initialize_project_config(config: dict, **kwargs):
+    venv_dir = kwargs.get("venv", "venv")
+    conf_gen = YamlConfigGenerator("pipcx.yaml")
+    config.update(venv=venv_dir)
+    conf_gen.append(**config)
+    conf_gen.generate()
+    # Installs virtualenv library
+    install_venv()
+    # Initializes virtualenv
+    initialize_venv(venv_dir)
+    # Activates virtualenv
+    activate_venv(venv_dir)
+    # Initialize project file
 
 
 class Command(Base):
@@ -10,13 +35,12 @@ class Command(Base):
 
     def add_argument(self, parser: ArgumentParser):
         parser.add_argument(
-            "virtualenv",
+            "project_name",
             nargs='?',
-            metavar="virtualenv",
+            metavar="project_name",
             type=str,
-            help="Name of virtualenv directory",
+            help="Name of Project",
             default=''
-
         )
 
         parser.add_argument(
@@ -24,25 +48,54 @@ class Command(Base):
         )
 
     def execute(self, *args, **kwargs):
-        venv_dir = kwargs.get("venv", None)
-        virtual_env = kwargs.get("virtualenv", None)
-
-        # Only one argument is allowed
-        if virtual_env and venv_dir:
-            raise InvalidCommandError("2 Virtualenv directory name provided, "
-                                      "use either pipcx init 'name' or pipcx init --venv='name'")
-
-        elif virtual_env and not venv_dir:
-            name = virtual_env
-
-        elif venv_dir and not virtual_env:
-            name = venv_dir
-
-        else:
-            name = 'venv'
+        project_name = kwargs.get("project_name", None)
+        spinner = Spinner()
+        input_handler = PromptHandler()
+        if not project_name:
+            input_handler.add_input(
+                Input(
+                    name="project_name",
+                    title="Project Name",
+                    default="project",
+                    required=False
+                )
+            )
+        input_handler.add_inputs(
+            Input(
+                name="author",
+                title="Author",
+                default="Author",
+                required=False
+            ),
+            Input(
+                name="email",
+                title="Email",
+                default="email@email.com",
+                required=False
+            ),
+            Input(
+                name="license",
+                title="license",
+                default="MIT",
+                required=False
+            ),
+        )
+        config = input_handler.prompt()
+        initialize_project_config(config, **kwargs)
+        spinner.start()
         # Installs virtualenv library
-        install_venv()
-        # Initializes virtualenv
-        initialize_venv(name)
-        # Activates virtualenv
-        activate_venv(name)
+        project_name = input_handler.answers.project_name
+        try:
+            os.mkdir(project_name)
+        except FileExistsError:
+            pass
+
+        with open(f"{project_name}/__init__.py", "w"):
+            pass
+
+        if not os.path.exists(f"{os.getcwd()}/main.py"):
+            with open(f"{project_name}/main.py", "w") as file:
+                file.write(main_file)
+
+        spinner.stop()
+        self.output("\nProject initialization complete")
