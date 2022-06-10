789851dba663b54a4697e6dd5cec5067ed6f15b2
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-10 21:52:35 +0600

    :art: Multiple test cases

--- test/commands/test_init.py
+++ test/commands/test_init.py
@@ -7,6 +7,7 @@ import pytest
 from pipcx.commands.init import Command as InitCommand
 import shutil
 
+from pipcx.main import CLI
 from test.utils import make_multiple_inputs
 
 
@@ -22,16 +23,70 @@ def path():
     return path
 
 
+def safe_remove_dir(dir_path):
+    try:
+        shutil.rmtree(dir_path)
+    except FileNotFoundError:
+        pass
+
+
+def safe_remove_file(file_path):
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        pass
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    path = Path(__file__).resolve().parent / 'temp'
+    os.chdir(path=path)
+    safe_remove_dir("testproject")
+    safe_remove_dir("testvenv")
+    safe_remove_file("pipcx.yaml")
+    safe_remove_file("pytest.yaml")
+
+
 def test_init_with_option_args(init_command, path, monkeypatch):
+    # Set input parameter
     monkeypatch.setattr("builtins.input", make_multiple_inputs(
-        deque(["my_proj", "mit", "author", "name@name.com"])))
+        deque(["testproject", "mit", "author", "name@name.com"])))
+    # Run the command
     init_command.run(["pipcx", "init", "--venv=testvenv"])
+    # Check directories
     assert os.path.isdir("testvenv")
-    assert os.path.isdir("my_proj")
+    assert os.path.isdir("testproject")
     assert os.path.isfile("pipcx.yaml")
-    assert os.path.isfile("my_proj/main.py")
+    assert os.path.isfile("testproject/main.py")
+    # Remove test files
     shutil.rmtree(path / 'testvenv')
-    shutil.rmtree(path / 'my_proj')
+    shutil.rmtree(path / 'testproject')
     os.remove("pipcx.yaml")
 
 
+def test_init_with_predefined_files(path, monkeypatch):
+    cli = CLI(["pipcx", "init", "--venv=testvenv"])
+    # Set input parameter
+    monkeypatch.setattr("builtins.input", make_multiple_inputs(
+        deque(["testproject", "mit", "author", "name@name.com"])))
+    monkeypatch.setattr("sys.argv", ["pipcx", "init", "--venv=testvenv"])
+    # Run the command
+    os.mkdir("testproject")
+    with open("testproject/main.py", "w") as file:
+        file.write("print('Hello World')")
+        file.close()
+
+    cli.execute()
+    # Check directories
+    assert os.path.isdir("testvenv")
+    assert os.path.isdir("testproject")
+    assert os.path.isfile("pipcx.yaml")
+    assert os.path.isfile("testproject/main.py")
+    # Read if file has not been updated
+    with open("testproject/main.py", "r") as file:
+        assert "Hello World" in file.read()
+        file.close()
+    # Remove test files
+    shutil.rmtree(path / 'testvenv')
+    shutil.rmtree(path / 'testproject')
+    os.remove("pipcx.yaml")
