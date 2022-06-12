885efac43d6953d4156ef86476297165dfed6e71
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-13 00:18:34 +0600

    :art: Code refactor

--- pipcx/commands/init.py
+++ pipcx/commands/init.py
@@ -30,40 +30,22 @@ import os
 from argparse import ArgumentParser
 
 from pipcx.base import Base
-from pipcx.io import PromptHandler, Input
-from pipcx.utils import YAMLConfig
-from pipcx.utils.spinner import Spinner
-from pipcx.utils.venv import install_venv, initialize_venv, activate_venv
+from pipcx.io import Input
+from pipcx.schema import get_project_name, initialize_project_config
 
-MAIN_FILE = """
-# Generated with pipcx
+MAIN_FILE = ""
+"""# Generated with pipcx
 
 if __name__ == "__main__":
-   print("Egg and spam")
+    print("Egg and spam")
 """
-
-
-def initialize_project_config(config: dict, **kwargs):
-    """
-    Creates base project config file and virtualenv
-    """
-    venv_dir = kwargs.get("venv", "venv")
-    conf_gen = YAMLConfig("pipcx.yaml", **config)
-    conf_gen.append(venv=venv_dir)
-    conf_gen.save()
-    # Installs virtualenv library
-    install_venv()
-    # Initializes virtualenv
-    initialize_venv(venv_dir)
-    # Activates virtualenv
-    activate_venv(venv_dir)
-    # Initialize project file
 
 
 class Command(Base):
     """
     Init command class
     """
+
     short_description = "Initialize Virtual environment"
 
     def add_argument(self, parser: ArgumentParser):
@@ -71,39 +53,35 @@ class Command(Base):
         Adds custom argument
         """
         parser.add_argument(
-            "project_name",
+            "name",
             nargs='?',
-            metavar="project_name",
+            metavar="name",
             type=str,
             help="Name of Project",
             default=''
         )
 
         parser.add_argument(
-            '--venv', help="Virtual Environment directory name"
+            '--venv', help="Virtual Environment directory name", default="venv"
         )
 
-        parser.add_argument(
-            '--no-input', help="Will not take any input", action="store_true"
-        )
+    def add_input(self, handler, **options):
+        """
+        Handles prompt input
+        """
+        project_name = options.get("name", None)
 
-    def execute(self, *args, **kwargs):
-        """
-        Inherited execute method
-        """
-        project_name = kwargs.get("project_name", None)
-        spinner = Spinner()
-        input_handler = PromptHandler()
         if not project_name:
-            input_handler.add_input(
+            handler.add_input(
                 Input(
-                    name="project_name",
+                    name="name",
                     title="Project Name",
-                    default="project",
+                    default="src",
                     required=False
                 )
             )
-        input_handler.add_inputs(
+
+        handler.add_inputs(
             Input(
                 name="author",
                 title="Author",
@@ -123,11 +101,21 @@ class Command(Base):
                 required=False
             ),
         )
-        config = input_handler.prompt()
-        initialize_project_config(config, **kwargs)
-        spinner.start()
-        # Installs virtualenv library
-        project_name = input_handler.answers.project_name
+
+    def format_arguments(self, **kwargs):
+        """
+        Formats command arguments and input
+        """
+        project_name = get_project_name(**kwargs)
+        kwargs.update(name=project_name)
+        return kwargs
+
+    @staticmethod
+    def handle_directory(**kwargs):
+        """
+        Handle directory creation
+        """
+        project_name = kwargs.get("name")
         try:
             os.mkdir(project_name)
         except FileExistsError:
@@ -141,5 +129,11 @@ class Command(Base):
             with open(f"{project_name}/main.py", "w", encoding="utf-8") as file:
                 file.write(MAIN_FILE)
 
-        spinner.stop()
+    def execute(self, **command_data):
+        """
+        Inherited execute method
+        """
+        initialize_project_config(**command_data)
+        # Installs virtualenv library
+        self.handle_directory(**command_data)
         self.output("\nProject initialization complete")
