817647621c1ee2541eb17df98ff8467d21a79d37
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-13 00:18:32 +0600

    :art: Code refactor

--- test/utils.py
+++ test/utils.py
@@ -1,8 +1,27 @@
-def make_multiple_inputs(inputs):
+import os
+import shutil
+
+
+def make_multiple_inputs(inputs: list):
     """ provides a function to call for every input requested. """
 
     def next_input(_):
         """ provides the first item in the list. """
-        return inputs.popleft()
+        return inputs.pop(0)
 
     return next_input
+
+
+def safe_remove_dir(dir_path):
+    try:
+        shutil.rmtree(dir_path)
+    except FileNotFoundError:
+        pass
+
+
+def safe_remove_file(file_path):
+    try:
+        os.remove(file_path)
+    except FileNotFoundError:
+        pass
+
