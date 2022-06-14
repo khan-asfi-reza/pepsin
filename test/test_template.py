84587503dc5d961f10530f3ad2149fd6e0eff68d
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-15 00:41:32 +0600

    :white_check_mark: Test Template does not exist

---
+++ test/test_template.py
@@ -0,0 +1,12 @@
+from pipcx.template import Template, TemplateDoesNotExistError
+
+
+def test_template_does_not_exist():
+    template = Template(template_name="TEST")
+
+    try:
+        template.read()
+
+    except TemplateDoesNotExistError as error:
+        assert isinstance(error, TemplateDoesNotExistError)
+
