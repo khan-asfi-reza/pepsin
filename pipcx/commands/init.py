8ccfa2cc1a324ed83e1b6adc83962842a1bce1d7
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-18 19:55:25 +0600

    :fire: Black linting

--- pipcx/commands/init.py
+++ pipcx/commands/init.py
@@ -30,8 +30,8 @@ import os
 from argparse import ArgumentParser
 
 from pipcx.base import Base
+from pipcx.config import PipcxConfig, get_project_name
 from pipcx.io import Input
-from pipcx.config import get_project_name, PipcxConfig
 from pipcx.pyhandler import PyHandler
 from pipcx.template import Template, TemplateList
 
@@ -49,6 +49,8 @@ class Command(Base):
     """
 
     short_description = "Initialize Virtual environment"
+    help = """Initialize virtual environment, project data and file
+    """
 
     def add_argument(self, parser: ArgumentParser):
         """
@@ -56,15 +58,15 @@ class Command(Base):
         """
         parser.add_argument(
             "name",
-            nargs='?',
+            nargs="?",
             metavar="name",
             type=str,
             help="Name of Project",
-            default=''
+            default="",
         )
 
         parser.add_argument(
-            '--venv', help="Virtual Environment directory name", default="venv"
+            "--venv", help="Virtual Environment directory name", default="venv"
         )
 
     def add_input(self, handler, **options):
@@ -75,33 +77,15 @@ class Command(Base):
 
         if not project_name:
             handler.add_input(
-                Input(
-                    name="name",
-                    title="Project Name",
-                    default="src",
-                    required=False
-                )
+                Input(name="name", title="Project Name", default="src", required=False)
             )
 
         handler.add_inputs(
+            Input(name="author", title="Author", default="Author", required=False),
             Input(
-                name="author",
-                title="Author",
-                default="Author",
-                required=False
+                name="email", title="Email", default="email@email.com", required=False
             ),
-            Input(
-                name="email",
-                title="Email",
-                default="email@email.com",
-                required=False
-            ),
-            Input(
-                name="license",
-                title="license",
-                default="MIT",
-                required=False
-            ),
+            Input(name="license", title="license", default="MIT", required=False),
         )
 
     def format_arguments(self, **kwargs):
@@ -136,7 +120,10 @@ class Command(Base):
         """
         template_list.add_template(
             Template(template_name=".gitignore"),
-            Template(template_name="Readme.MD", context={"name": self.command_data.get("name")})
+            Template(
+                template_name="Readme.MD",
+                context={"name": self.command_data.get("name")},
+            ),
         )
 
     def execute(self):
