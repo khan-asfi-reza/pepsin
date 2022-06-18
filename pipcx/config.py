aeac3ef759dbbc18dd8ce3a05a7d3debe2110599
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-18 19:55:18 +0600

    :fire: Black linting

--- pipcx/config.py
+++ pipcx/config.py
@@ -22,21 +22,15 @@ def format_attr(attr):
     """
     Formats attribute and return empty string if Null
     """
-    return '' if not attr else attr
+    return "" if not attr else attr
 
 
 class PipcxConfig:
     """
     PIPCX Config blueprint
     """
-    __slots__ = ["name",
-                 "venv",
-                 "author",
-                 "license",
-                 "libraries",
-                 "scripts",
-                 "conf"
-                 ]
+
+    __slots__ = ["name", "venv", "author", "license", "libraries", "scripts", "conf"]
 
     def __init__(self):
         self.libraries = []
@@ -70,7 +64,7 @@ class PipcxConfig:
         # Update libraries
         self.update_libraries(libs)
         for key in kwargs:
-            if key != 'libraries' and key in self.get_slots():
+            if key != "libraries" and key in self.get_slots():
                 setattr(self, key, kwargs.get(key))
         # Update configuration and save to yaml
         self.conf.append(**self.format_config())
@@ -113,6 +107,8 @@ def handle_failed_libs(failed):
     """
     Creates or updates failed installation log
     """
-    ftext = f'# Module Installation Failed {datetime.now().strftime("%d %B %Y | %H:%M:%S")}'
+    ftext = (
+        f'# Module Installation Failed {datetime.now().strftime("%d %B %Y | %H:%M:%S")}'
+    )
     failed.insert(0, ftext)
     update_file("pipcx.failed.log", "\n".join(failed))
