ae177c71bf4affb9254ac20e6dbc39940cf3837c
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-18 19:56:04 +0600

    :fire: Black linting

--- pipcx/utils/spinner.py
+++ pipcx/utils/spinner.py
@@ -2,33 +2,34 @@
 Ref: https://gist.github.com/nicodebo/b68034445da9be238e706a2d8166cf79
 """
 
-from enum import Enum
 import itertools
 import sys
-import time
 import threading
+import time
+from enum import Enum
 
 
 class Sequence(Enum):
     """Enumeration of spinner sequence
     Ref: https://stackoverflow.com/questions/2685435/cooler-ascii-spinners
     """
-    BASIC = ['-', '/', '|', '\\']
-    ARROW = ['←', '↖', '↑', '↗', '→', '↘', '↓', '↙']
-    VERT_BAR = ['▁', '▃', '▄', '▅', '▆', '▇', '█', '▇', '▆', '▅', '▄', '▃']
-    HORIZ_BAR = ['▉', '▊', '▋', '▌', '▍', '▎', '▏', '▎', '▍', '▌', '▋', '▊', '▉']
-    SPIN_RECT = ['▖', '▘', '▝', '▗']
-    ELAST_BAR = ['▌', '▀', '▐▄']
-    TETRIS = ['┤', '┘', '┴', '└', '├', '┌', '┬', '┐']
-    TRIANGLE = ['◢', '◣', '◤', '◥']
-    SQUARE_QRT = ['◰', '◳', '◲', '◱']
-    CIRCLE_QRT = ['◴', '◷', '◶', '◵']
-    CIRCLE_HLF = ['◐', '◓', '◑', '◒']
-    BALLOON = ['.', 'o', 'O', '@', '*']
-    BLINK = ['◡◡', '⊙⊙', '◠◠']
-    TURN = ['◜ ', ' ◝', ' ◞', '◟ ']
-    LOSANGE = ['◇', '◈', '◆']
-    BRAILLE = ['⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷']
+
+    BASIC = ["-", "/", "|", "\\"]
+    ARROW = ["←", "↖", "↑", "↗", "→", "↘", "↓", "↙"]
+    VERT_BAR = ["▁", "▃", "▄", "▅", "▆", "▇", "█", "▇", "▆", "▅", "▄", "▃"]
+    HORIZ_BAR = ["▉", "▊", "▋", "▌", "▍", "▎", "▏", "▎", "▍", "▌", "▋", "▊", "▉"]
+    SPIN_RECT = ["▖", "▘", "▝", "▗"]
+    ELAST_BAR = ["▌", "▀", "▐▄"]
+    TETRIS = ["┤", "┘", "┴", "└", "├", "┌", "┬", "┐"]
+    TRIANGLE = ["◢", "◣", "◤", "◥"]
+    SQUARE_QRT = ["◰", "◳", "◲", "◱"]
+    CIRCLE_QRT = ["◴", "◷", "◶", "◵"]
+    CIRCLE_HLF = ["◐", "◓", "◑", "◒"]
+    BALLOON = [".", "o", "O", "@", "*"]
+    BLINK = ["◡◡", "⊙⊙", "◠◠"]
+    TURN = ["◜ ", " ◝", " ◞", "◟ "]
+    LOSANGE = ["◇", "◈", "◆"]
+    BRAILLE = ["⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"]
 
     # def describe(self):
     #     return self.name, self.value
@@ -42,8 +43,7 @@ class Sequence(Enum):
 
 
 class Spinner:
-    """A shell spinner
-    """
+    """A shell spinner"""
 
     def __init__(self, message="", interval=0.25, sequence="HORIZ_BAR", offset=1):
         self.stop_running = threading.Event()
@@ -89,8 +89,8 @@ class Spinner:
                 cur_mess = next(self.spinner_cycle)
             else:
                 cur_mess = f"{next(self.spinner_cycle)} {self.message}"
-            cur_mess = cur_mess.rjust(len(cur_mess) + self.offset, ' ')
+            cur_mess = cur_mess.rjust(len(cur_mess) + self.offset, " ")
             sys.stdout.write(cur_mess)
             sys.stdout.flush()
             time.sleep(self.interval)
-            sys.stdout.write('\b' * len(cur_mess))
+            sys.stdout.write("\b" * len(cur_mess))
