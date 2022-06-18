e9fd77f26a3c710aaaa25091120d67c401cd4789
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-18 21:15:53 +0600

    :sparkles: Install library with 'i' command

---
+++ pipcx/commands/i.py
@@ -0,0 +1,10 @@
+"""
+Install/Add Alternative
+"""
+from pipcx.commands.install import Command as InstallCommand
+
+
+class Command(InstallCommand):
+    """
+    Install Command alternative
+    """
