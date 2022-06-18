c2cb19f035c0d0c032c243cb5d6edc930b2c1d2b
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-18 20:45:03 +0600

    :art: Code format Fix Black + isort

--- test/commands/test_init.py
+++ test/commands/test_init.py
@@ -1,10 +1,10 @@
 import os
+from test.utils import command_path, make_multiple_inputs
 
 import pytest
 
 from pipcx.commands.init import Command as InitCommand
 from pipcx.main import CLI
-from test.utils import make_multiple_inputs, command_path
 
 
 @pytest.fixture
@@ -14,8 +14,12 @@ def init_command():
 
 def test_init_with_option_args(init_command, monkeypatch):
     # Set input parameter
-    monkeypatch.setattr("builtins.input", make_multiple_inputs(
-        ["testproject", "mit", "author", "name@name.com"]))
+    monkeypatch.setattr(
+        "builtins.input",
+        make_multiple_inputs(
+            ["testproject", "mit", "author", "name@name.com"]
+        ),
+    )
     # Run the command
     init_command.run(["pipcx", "init", "--venv=testvenv"])
     # Check directories
@@ -28,7 +32,9 @@ def test_init_with_option_args(init_command, monkeypatch):
 
 
 def test_init_with_predefined_files_no_input(init_command, monkeypatch):
-    cli = CLI(["pipcx", "init", "testproject", "--venv=testvenv", "--no-input"])
+    cli = CLI(
+        ["pipcx", "init", "testproject", "--venv=testvenv", "--no-input"]
+    )
     # Set input parameter
     os.mkdir("testproject")
     with open("testproject/main.py", "w") as file:
@@ -52,8 +58,10 @@ def test_init_with_predefined_files_no_input(init_command, monkeypatch):
 def test_init_with_predefined_files(monkeypatch):
     cli = CLI(["pipcx", "init", "testproject", "--venv=testvenv"])
     # Set input parameter
-    monkeypatch.setattr("builtins.input", make_multiple_inputs(
-        ["mit", "author", "name@name.com"]))
+    monkeypatch.setattr(
+        "builtins.input",
+        make_multiple_inputs(["mit", "author", "name@name.com"]),
+    )
 
     os.mkdir("testproject")
     with open("testproject/main.py", "w") as file:
