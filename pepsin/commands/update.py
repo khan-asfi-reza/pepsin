16369bc9587e8026e26fc1e7821f408dc022a873
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-20 15:14:01 +0600

    :sparkles: Added help text

--- pepsin/commands/update.py
+++ pepsin/commands/update.py
@@ -8,3 +8,9 @@ class Command(UpgradeCommand):
     """
     Update command / Upgrade Command
     """
+
+    short_description = "Update or upgrade library"
+    help = """Upgrades a particular or multiple libraries
+        `$pepsin update <library>`
+        Example: `$pepsin update django`
+        """
