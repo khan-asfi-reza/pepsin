9f57ceffe97b958e977d7f6db288047efae76ee6
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-24 01:52:59 +0600

    :sparkles: Command search functions moved to Base class

--- pepsin/utils/base.py
+++ pepsin/utils/base.py
@@ -2,48 +2,8 @@
 Contains Base utility that is required to run the pepsin CLI Class
 """
 import enum
-import functools
 import os
-import pkgutil
-from importlib import import_module
-from pathlib import Path
 from sys import platform
-
-from pepsin.base import Base
-
-ROOT = Path(__file__).resolve().parent
-
-
-def find_commands():
-    """
-    Finds commands located in the commands directory
-    """
-    function_directory = os.path.join(ROOT, "../commands")
-
-    return [
-        name
-        for _, name, is_pkg in pkgutil.iter_modules([function_directory])
-        if not is_pkg and not name.startswith("_")
-    ]
-
-
-def load_command_class(name) -> Base:
-    """
-    Every Command module has a Command class, which will be imported
-    :return Command Class
-    """
-    module = import_module(f"pepsin.commands.{name}")
-    return module.Command()
-
-
-@functools.lru_cache(maxsize=None)
-def get_commands():
-    """
-    Set of commands that will be returned
-    :return: set of commands
-    """
-    commands = set(find_commands())
-    return commands
 
 
 def check_file_exists(file):
