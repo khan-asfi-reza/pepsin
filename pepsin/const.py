d40da27874857664e8f5ef18d8afaca25c46991f
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-24 02:32:01 +0600

    :bug: CI/CD Test Fail fix

--- pepsin/const.py
+++ pepsin/const.py
@@ -1,10 +1,11 @@
 """
 Constants that will be used throughout the project
 """
+import os
 from pathlib import Path
 
 COMMAND_NOT_FOUND_ERROR = "use 'pepsin help' to find commands"
 PEPSIN_ROOT = Path(__file__).resolve().parent
 TEMPLATE_DIR = PEPSIN_ROOT / "templates"
-COMMAND_DIR = PEPSIN_ROOT / "commands"
+COMMAND_DIR = os.path.join(PEPSIN_ROOT, "commands")
 PIP_DL_LINK = "https://bootstrap.pypa.io/get-pip.py"
