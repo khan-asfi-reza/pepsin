b056679cd22d3d8ed100c2ea2fca3bdc38506b43
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-06-29 18:18:39 +0600

    :sparkles: Directory Template Class

--- pepsin/template.py
+++ pepsin/template.py
@@ -2,22 +2,59 @@
 Template handling module
 """
 import os
+from abc import ABC, abstractmethod
+from pathlib import Path
 from typing import List
 
 from pepsin.const import TEMPLATE_DIR
 from pepsin.error import TemplateDoesNotExistError
+from pepsin.utils import check_dir_exists, write_file
 
 
-class Template:
+class BaseTemplate(ABC):
+    def __init__(self):
+        self.directory = os.getcwd()
+
+    @abstractmethod
+    def handle(self):
+        """
+        Saves templates in the working directory
+        Returns:
+        """
+
+    def save(self):
+        """
+        Calls the save method of the child class
+        and creates directory safely
+        Returns:
+        """
+        if not check_dir_exists(self.directory):
+            Path(self.directory).mkdir(parents=True, exist_ok=True)
+        self.handle()
+
+
+class Template(BaseTemplate):
     """
     Template class to save and format Template Files
     """
 
-    def __init__(self, template_name, save_as=None, context=None):
+    def __init__(
+        self, template_name="", directory="", save_as=None, context=None
+    ):
+        """
+
+        Args:
+            template_name: Name of the template stored in the templates directory
+            directory: Given directory, where to store the file
+            save_as: Name of the file that will be saved, default as template name
+            context: Data to fill in the template
+        """
+        super().__init__()
         self.template_name = template_name
         self.file = ""
         self.save_as = template_name if not save_as else save_as
         self.context = {} if not context else context
+        self.directory = f"{os.getcwd()}/{directory}"
 
     def read(self):
         """
@@ -54,28 +91,70 @@ class Template:
         for key, val in self.context.items():
             self.file.replace(f"${key}", val)
 
-    def save(self):
+    def handle(self):
         """
         Saves templates in the working directory
         Returns:
         """
         self.read()
         self.format()
-        working_dir = os.getcwd()
-        if not os.path.isfile(os.path.join(working_dir, self.save_as)):
-            with open(
-                f"{working_dir}/{self.save_as}", "w", encoding="utf-8"
-            ) as file:
-                file.write(self.file)
+        if not os.path.isfile(os.path.join(self.directory, self.save_as)):
+            write_file(f"{self.directory}/{self.save_as}", self.file)
+
+
+class TemplateDirectory(BaseTemplate):
+    """
+    Handles only directory creation
+    """
+
+    def __init__(self, name):
+        """
+        Creates nested directory
+        Args:
+            name: name of the directory
+        """
+        super().__init__()
+        self.directory = f"{os.getcwd()}/{name}"
+
+    def handle(self):
+        """
+        Creates directory via the parent run_save method
+        Returns:
+
+        """
+
+
+class TemplateFromString(BaseTemplate):
+    """
+    Creates file in the given directory without a given template
+    and from text string
+    """
+
+    def __init__(self, name, text, directory=""):
+        """
+
+        Args:
+            name: Name of the file
+            text: Text to be in the file
+            directory: Directory where to store the file
+        """
+        super().__init__()
+        self.directory = f"{os.getcwd()}/{directory}"
+        self.name = name
+        self.text = text
+
+    def handle(self):
+        write_file(f"{self.directory}/{self.name}", self.text)
 
 
 class TemplateList:
     """
-    Template List DataStructure
+    Template List DataStructure,
+    to handle all the given Templates
     """
 
     def __init__(self):
-        self.templates: List[Template] = []
+        self.templates: List[BaseTemplate] = []
 
     def add_template(self, *args):
         """
